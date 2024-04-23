b2684edb4987cef343c96d1462bb5fff29dcec6d
FelipeTiepo <logonrmlocal@fiap.com.br>
2024-04-23 11:54:22 -0300

    adicionando a API, criando o gráfico e pegando os valores e o tempo em tempo real

---
+++ script/imprimeCotacao.js
@@ -0,0 +1,12 @@
+const lista = document.querySelector('[data-lista]')
+
+function imprimeCotacao(nome, valor){
+    lista.innerHTML = ''
+
+    for(let multiplicador = 1; multiplicador <= 1000; multiplicador *= 10) {
+        const listaItem = document.createElement('li')
+        listaItem.innerHTML = `${multiplicador} ${nome}: R$${(valor * multiplicador.toFixed(2))}`
+        //APPENDCHILD faz que o filho(listaItem) entre para o pai(lista)
+        lista.appendChild(listaItem)
+    }
+}
